9a2ae9478a55e3fd539f20539c007bcae998bc90
Matt Di Pasquale <liveloveprosper@gmail.com>
2011-09-20 22:01:42 -0400

    Clean things up.

--- VenmoHTTPTests/VenmoHTTPTests.m
+++ VenmoHTTPTests/VenmoHTTPTests.m
@@ -1,31 +1,14 @@
-//
-//  VenmoHTTPTests.m
-//  VenmoHTTPTests
-//
-//  Created by John Graham on 9/20/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface VenmoHTTPTests : SenTestCase
+
+@end
 
 #import "VenmoHTTPTests.h"
 
 @implementation VenmoHTTPTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
+- (void)testExample {
     STFail(@"Unit tests are not implemented yet in VenmoHTTPTests");
 }
 
