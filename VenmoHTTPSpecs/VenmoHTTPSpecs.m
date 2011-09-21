dfd772340b64cdad8d189bb3914876e4012fcff8
Matt Di Pasquale <liveloveprosper@gmail.com>
2011-09-20 22:25:34 -0400

    Rename VenmoHTTPTests to VenmoHTTPSpecs.

---
+++ VenmoHTTPSpecs/VenmoHTTPSpecs.m
@@ -0,0 +1,13 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface VenmoHTTPSpecs : SenTestCase
+
+@end
+
+@implementation VenmoHTTPSpecs
+
+- (void)testExample {
+    STFail(@"Unit tests are not implemented yet in VenmoHTTPSpecs");
+}
+
+@end
